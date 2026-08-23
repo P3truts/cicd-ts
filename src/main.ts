f9e228f51a03a7f03a41f39307ebff60864bdca5
P3truts <obreja.petrut@gmail.com>
2026-08-23 14:35:53 +0300

    IA: update main

--- src/main.ts
+++ src/main.ts
@@ -29,10 +29,6 @@ app.use(
   }),
 );
 
-function unused() {
-  // break eslint
-}
-
 app.use("/", express.static(path.join(__dirname, config.api.filepathRoot)));
 
 const v1Router = express.Router();
